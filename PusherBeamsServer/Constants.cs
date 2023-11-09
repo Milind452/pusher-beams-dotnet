2bb493878a61f0767d814b08177f92fbe4f47e41
Milind452 <milind.mohapatra@gmail.com>
2023-11-09 12:27:52 +0530

    add more constants

--- PusherBeamsServer/Constants.cs
+++ PusherBeamsServer/Constants.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace PusherBeamsServer;
 
 internal class Constants
@@ -6,4 +8,8 @@ internal class Constants
     public const int INTEREST_STRING_MAX_LENGTH = 164;
     public const int USERS_ARRAY_MAX_LENGTH = 1000;
     public const int INTEREST_ARRAY_MAX_LENGTH = 100;
+
+    public const string SDK_VERSION = "1.0.0";
+
+    public const string INTERESTS_REGEX = "^[_-=|@|,|.|;|[A-Z]|[a-z]|[0-9]]*$";
 }
