4d2e086b823e699e5017a733270a3c8447daa776
Milind452 <milind.mohapatra@gmail.com>
2023-11-09 12:16:44 +0530

    Add app constants

---
+++ PusherBeamsServer/Constants.cs
@@ -0,0 +1,9 @@
+namespace PusherBeamsServer;
+
+internal class Constants
+{
+    public const int USERS_STRING_MAX_LENGTH = 164;
+    public const int INTEREST_STRING_MAX_LENGTH = 164;
+    public const int USERS_ARRAY_MAX_LENGTH = 1000;
+    public const int INTEREST_ARRAY_MAX_LENGTH = 100;
+}
